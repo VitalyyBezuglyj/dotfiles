069bc4739ba8cedfdcd0b793b48dacb28ae2582b
Vitaly Bezuglyj <vitaly.bezugly@atom.team>
2024-09-25 21:55:56 +0300

    Fix switch kb issue

--- dot_config/hypr/scripts/executable_LockScreen.sh
+++ dot_config/hypr/scripts/executable_LockScreen.sh
@@ -2,6 +2,10 @@
 # /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 
 # For Hyprlock
+us_id=0
 
+notify-send "Layout: US"
+sleep 1
+hyprctl switchxkblayout all $us_id
 pidof hyprlock || hyprlock -q 
 
